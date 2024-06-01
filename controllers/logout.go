eb8ec56c764e9c134c4ad649072dbb02c6aa6e17
Daiki Kim <kimdaiki5021@gmail.com>
2024-06-01 10:02:21 +0900

    Complete logout.go

--- controllers/logout.go
+++ controllers/logout.go
@@ -1,1 +1,16 @@
 package controllers
+
+import (
+	"awesomeProject/auth"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Logout(c *gin.Context) {
+	err := auth.ClearSession(c)
+	if err != nil {
+		c.JSON(500, gin.H{"Error": err.Error()})
+		return
+	}
+	c.JSON(200, gin.H{"Message": "logout success"})
+}

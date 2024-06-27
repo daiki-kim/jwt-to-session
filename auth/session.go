f3290eab49e22e3fb6782b5158d2c08e2afb004d
Daiki Kim <kimdaiki5021@gmail.com>
2024-06-28 07:15:22 +0900

    Change session package from gorilla to gin

--- auth/session.go
+++ auth/session.go
@@ -4,12 +4,12 @@ import (
 	"errors"
 	"log"
 
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/sessions"
 )
 
 var (
-	Store = sessions.NewCookieStore([]byte("secret"))
+	Store sessions.Store
 )
 
 const (
@@ -17,35 +17,41 @@ const (
 	SessionName = "alamoa"
 )
 
+func SetStore(store sessions.Store) {
+	Store = store
+}
+
 func SetSession(c *gin.Context, email string) error {
-	session, _ := Store.Get(c.Request, SessionName)
-	session.Options = &sessions.Options{
+	session := sessions.Default(c)
+	session.Options(sessions.Options{
 		Path:     "/",
-		MaxAge:   60,
+		MaxAge:   60 * 60,
 		Secure:   false,
 		HttpOnly: true,
-	}
-	log.Printf("SetSession: %v", session.Values)
-	session.Values[SessionKey] = email
-	return session.Save(c.Request, c.Writer)
+	})
+	session.Set(SessionKey, email)
+	log.Printf("SetSession: %v", email)
+	return session.Save()
 }
 
 func GetSession(c *gin.Context) (string, error) {
-	session, _ := Store.Get(c.Request, SessionName)
-	log.Printf("GetSession: %v", session.Values)
-	user, ok := session.Values[SessionKey].(string)
+	session := sessions.Default(c)
+	email, ok := session.Get(SessionKey).(string)
 	if !ok {
 		return "", errors.New("session not found")
 	}
-	return user, nil
+	log.Printf("GetSession: %v", email)
+	return email, nil
 }
 
 func ClearSession(c *gin.Context) error {
-	session, _ := Store.Get(c.Request, SessionName)
-	log.Printf("ClearSession before: %v", session.Values)
-	session.Options.MaxAge = -1
-	delete(session.Values, SessionKey)
-	err := session.Save(c.Request, c.Writer)
-	log.Printf("ClearSession after: %v", session.Values)
+	session := sessions.Default(c)
+	session.Options(sessions.Options{
+		Path:   "/",
+		MaxAge: -1,
+	})
+	session.Delete(SessionKey)
+	err := session.Save()
+	log.Println("Cleared session")
 	return err
 }
